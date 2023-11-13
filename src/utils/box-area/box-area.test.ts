76fedff3a13a679838053f183ad6adebf3b7f90b
rkterungwa16 <terungwakombol@gmail.com>
2023-11-13 20:02:34 +0100

    add utils for box area and breakpoint

---
+++ src/utils/box-area/box-area.test.ts
@@ -0,0 +1,55 @@
+import { generateBoxAreaStyle, generateComponentBoxAreaStyle } from "./box-area";
+
+describe("Box area util", () => {
+  it("should render padding styles", () => {
+    console.log(generateBoxAreaStyle("px-2"));
+    expect(generateBoxAreaStyle("p-2")).toEqual("padding: 0.5rem;");
+    expect(generateBoxAreaStyle("pt-2")).toEqual("padding-top: 0.5rem;");
+    expect(generateBoxAreaStyle("pb-2")).toEqual("padding-bottom: 0.5rem;");
+    expect(generateBoxAreaStyle("pl-2")).toEqual("padding-left: 0.5rem;");
+    expect(generateBoxAreaStyle("pr-2")).toEqual("padding-right: 0.5rem;");
+    expect(generateBoxAreaStyle("px-2").includes("padding-right: 0.5rem;")).toEqual(true);
+    expect(generateBoxAreaStyle("px-2").includes("padding-left: 0.5rem;")).toEqual(true);
+  });
+
+  it("should render margin styles", () => {
+    expect(generateBoxAreaStyle("m-2")).toEqual("margin: 0.5rem;");
+    expect(generateBoxAreaStyle("mt-2")).toEqual("margin-top: 0.5rem;");
+    expect(generateBoxAreaStyle("mb-2")).toEqual("margin-bottom: 0.5rem;");
+    expect(generateBoxAreaStyle("ml-2")).toEqual("margin-left: 0.5rem;");
+    expect(generateBoxAreaStyle("mr-2")).toEqual("margin-right: 0.5rem;");
+    expect(generateBoxAreaStyle("mx-2").includes("margin-right: 0.5rem;")).toEqual(true);
+    expect(generateBoxAreaStyle("mx-2").includes("margin-left: 0.5rem;")).toEqual(true);
+  });
+
+  it("should", () => {
+    expect(
+      generateComponentBoxAreaStyle({
+        p: "p-0",
+        px: "px-3",
+        py: "py-9"
+      }).includes("padding: 0rem;")
+    ).toEqual(true);
+    expect(
+      generateComponentBoxAreaStyle({
+        p: "p-0",
+        px: "px-3",
+        py: "py-9"
+      }).includes("padding-left: 1rem;")
+    ).toEqual(true);
+    expect(
+      generateComponentBoxAreaStyle({
+        p: "p-0",
+        px: "px-3",
+        py: "py-9"
+      }).includes("padding-right: 1rem;")
+    ).toEqual(true);
+    expect(
+      generateComponentBoxAreaStyle({
+        p: "p-0",
+        px: "px-3",
+        py: "py-9"
+      }).includes(" padding-top: 4rem;")
+    ).toEqual(true);
+  });
+});
---
+++ src/utils/box-area/box-area.ts
@@ -0,0 +1,64 @@
+import { BoxAreaProps } from "./types";
+
+
+// margin-bottom: 0;
+export const boxAreas: {
+  [x: string]: string;
+} = {
+  p: "padding",
+  m: "margin"
+};
+
+export const boxSides: {
+  [x: string]: string;
+} = {
+  t: "top",
+  b: "bottom",
+  l: "left",
+  r: "right",
+  x: "left right",
+  y: "top bottom"
+};
+
+const spacing: {
+  [x: number]: number;
+} = {
+  0: 0,
+  1: 0.25,
+  2: 0.5,
+  3: 1,
+  4: 1.5,
+  5: 2,
+  6: 2.5,
+  7: 3,
+  8: 3.5,
+  9: 4,
+  10: 4.5
+};
+
+export const generateBoxAreaStyle = (props: string) => {
+  const [boxAreaSide, spacingValue] = props.split("-");
+  const [boxArea, boxSide] = boxAreaSide.split("");
+  if (!boxSide) {
+    return `${boxAreas[boxArea]}: ${spacing[Number(spacingValue)]}rem;`;
+  }
+  if (boxSides[boxSide]?.split(" ").length === 2) {
+    const [first, second] = boxSides[boxSide].split(" ");
+    return `
+    ${boxAreas[boxArea]}-${first}: ${spacing[Number(spacingValue)]}rem;
+    ${boxAreas[boxArea]}-${second}: ${spacing[Number(spacingValue)]}rem;
+    `;
+  }
+  return `${boxAreas[boxArea]}-${boxSides[boxSide]}: ${spacing[Number(spacingValue)]}rem;`;
+};
+
+export const generateComponentBoxAreaStyle = (props: BoxAreaProps) => {
+  return Object.values(props)
+    .filter((_prop) => _prop)
+    .reduce((prev, curr) => {
+      return `
+      ${prev}
+      ${generateBoxAreaStyle(curr)}
+    `;
+    }, "");
+};
