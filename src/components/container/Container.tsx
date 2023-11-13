5adbfad4349b358b217f73149173c3f14894a6e6
rkterungwa16 <terungwakombol@gmail.com>
2023-11-13 21:31:59 +0100

    implement container component

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,15 @@
+import { forwardRef } from "react";
+import { StyledHeaderContainer } from "./styles";
+import { ContainerProps } from "./types";
+
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+  ({ children, ...others }, ref) => {
+    return (
+      <StyledHeaderContainer ref={ref} {...others}>
+        {children}
+      </StyledHeaderContainer>
+    );
+  },
+);
+
+Container.displayName = "HeaderContainer";
