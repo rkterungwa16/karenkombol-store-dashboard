d0f71f5a52e0ae2687f5e9894d5532c9733ab39c
rkterungwa16 <terungwakombol@gmail.com>
2023-11-19 11:26:06 +0100

    add padding props to sidenav component

--- src/components/app/sidebar/AppSidebar.tsx
+++ src/components/app/sidebar/AppSidebar.tsx
@@ -8,7 +8,7 @@ export const AppSidebar = () => {
       {({ currentOpenItem, handleSetCurrentOpenItem }) => {
         {
           return (
-            <SidebarNav>
+            <SidebarNav px="px-3">
               {navItems.map((_item) => {
                 if (_item.type === "item") {
                   const { component, to, icon: Icon, name } = _item;
@@ -26,15 +26,15 @@ export const AppSidebar = () => {
                       // }}
                       href={to}
                       key={name}
+                      {...(Icon && { icon: <Icon /> })}
                     >
-                      {Icon && <Icon />}
                       {name && name}
                     </Component>
                   );
                 }
 
                 if (_item.type === "group") {
-                  const { component, items, name, icon: Icon } = _item;
+                  const { component, items, name, icon: Icon, to } = _item;
                   const Component = component;
                   return (
                     <Component
@@ -43,16 +43,17 @@ export const AppSidebar = () => {
                       // disabled={false}
                       toggler={
                         <>
-                          {Icon && <Icon />}
                           {name && name}
                         </>
                       }
+                      {...(Icon && { icon: <Icon /> })}
                       key={name}
                       handleSetCurrentOpenItem={handleSetCurrentOpenItem}
                       visible={
                         currentOpenItem === name.toLowerCase() ? true : false
                       }
                       idx={name.toLowerCase()}
+                      href={to}
                     >
                       {items
                         ? items?.map((_item, index) => {
@@ -71,6 +72,7 @@ export const AppSidebar = () => {
                                 // }}
                                 href={to}
                                 key={index}
+                                pl="pl-4"
                               >
                                 {name && name}
                               </Component>
--- src/components/app/sidebar/sidebar-nav/SidebarNav.tsx
+++ src/components/app/sidebar/sidebar-nav/SidebarNav.tsx
@@ -1,22 +1,41 @@
 import styled from "styled-components";
 import { FC, ReactNode } from "react";
+import {
+  BoxAreaProps,
+  StyledBoxAreaProps,
+  generateComponentBoxAreaStyle,
+} from "../../../../utils/box-area";
+import { generateStyledComponentPropKeys } from "../../../../utils/styledComponentPropKeys";
+
+export interface StyledSidebarNavProps extends StyledBoxAreaProps {
+  children?: ReactNode;
+  $className?: string;
+}
 
 export const StyledSidebarNav = styled.ul`
   position: relative;
   display: flex;
   flex: 1 1;
   flex-direction: column;
-  padding: 0;
-  margin-bottom: 0;
   overflow-x: hidden;
   overflow-y: auto;
   list-style: none;
+  ${(props: StyledSidebarNavProps) => {
+    const style = `
+      ${generateComponentBoxAreaStyle({
+        ...props,
+      })}
+    `;
+    return style;
+  }}
 `;
 
-export type SidebarNavProps = {
+export interface SidebarNavProps extends BoxAreaProps {
   children?: ReactNode;
-};
+}
 
-export const SidebarNav: FC<SidebarNavProps> = ({
-  children,
-}) => <StyledSidebarNav>{children}</StyledSidebarNav>;
+export const SidebarNav: FC<SidebarNavProps> = ({ children, ...others }) => (
+  <StyledSidebarNav {...generateStyledComponentPropKeys(others)}>
+    {children}
+  </StyledSidebarNav>
+);
