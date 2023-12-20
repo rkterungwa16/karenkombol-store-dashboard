a4af626aec4707b8065241808a85619676e8ce96
rkterungwa16 <terungwakombol@gmail.com>
2023-12-20 11:35:39 +0100

    remove logs and redundant props

--- src/components/private-route/PrivateRoute.tsx
+++ src/components/private-route/PrivateRoute.tsx
@@ -4,11 +4,9 @@ import { ClientRoutes } from "../../routes/client";
 import { useSelector } from "react-redux";
 import { loginSelector } from "../../services/login/login-selector";
 
-export const PrivateRoute = ({ children }: { children: JSX.Element, path?: ClientRoutes }) => {
+export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const loginData = useSelector(loginSelector);
   const location = useLocation();
-  console.log('location__protected route', location);
-  console.log('loginData__protected route', loginData);
   const token = loginData?.token;
 
   if (!token) {
