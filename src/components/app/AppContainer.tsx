b998a37db473dc905590585020f83a54d62f93bc
rkterungwa16 <terungwakombol@gmail.com>
2023-12-28 05:47:46 +0100

    modify roles page to fetch roles when authenticated

--- src/components/app/AppContainer.tsx
+++ src/components/app/AppContainer.tsx
@@ -1,33 +1,12 @@
-import { FC, ReactNode, useCallback, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { FC, ReactNode } from "react";
 
 import { AppSidebar } from "./AppSidebar";
 import { Container } from "../container/Container";
-import { loginSelector } from "../../services/login/login-selector";
-import { AppDispatch } from "../../store";
-import { fetchRolesAsync } from "../../services/roles/roles-async";
 
 export type AppProps = {
   children?: ReactNode;
 };
 export const AppContainer: FC<AppProps> = ({ children }) => {
-  const dispatch = useDispatch<AppDispatch>();
-  const loginData = useSelector(loginSelector);
-
-  const handleNavigate = useCallback(() => {
-    if (loginData?.token) {
-      dispatch(fetchRolesAsync({
-        headers: {
-          authorization: `Bearer ${loginData.token}`,
-        }
-      }));
-    }
-  }, [loginData, dispatch]);
-
-  useEffect(() => {
-    handleNavigate();
-  }, [handleNavigate]);
-  console.log("loginData", loginData);
   return (
     <Container
       height="100vh"
--- src/pages/roles/index.tsx
+++ src/pages/roles/index.tsx
@@ -1,7 +1,32 @@
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { Content } from "../../components/app/content/Content";
 import AppContainer from "../../components/app/AppContainer";
 
+import { loginSelector } from "../../services/login/login-selector";
+import { fetchRolesAsync } from "../../services/roles/roles-async";
+import { AppDispatch } from "../../store";
+
 export const Roles = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const loginData = useSelector(loginSelector);
+
+  const handleFetchRoles = useCallback(() => {
+    if (loginData?.token) {
+      dispatch(
+        fetchRolesAsync({
+          headers: {
+            authorization: `Bearer ${loginData.token}`,
+          },
+        }),
+      );
+    }
+  }, [loginData, dispatch]);
+
+  useEffect(() => {
+    handleFetchRoles();
+  }, [handleFetchRoles]);
   return (
     <AppContainer>
       <Content></Content>
